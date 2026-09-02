b0b414800a2e9494f43322fea9d02647a2aea29f
Yash Raghuwanshi <yraghuwanshi124@gmail.com>
2026-09-02 12:39:21 +0530

    Complete college maintenance system

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://campus-maintainace-system-pmm9.vercel.app/",
+  baseURL: "https://campus-maintainace-system-pmm9.vercel.app",
 });
 
 export default api;

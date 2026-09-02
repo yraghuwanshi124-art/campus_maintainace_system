fdee0848f01e3c673aa821be55e3764ca1cfef9c
Yash Raghuwanshi <yraghuwanshi124@gmail.com>
2026-09-03 00:07:40 +0530

    fix production api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,29 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default api;
+
+// https://campus-maintainace-system-ly4tb7bsf-yraghuwanshi124-art.vercel.app/
+
+// import axios from "axios";
+
+// // const api = axios.create({
+// //   baseURL: "https://campus-maintainace-system-ly4tb7bsf-yraghuwanshi124-art.vercel.app/api",
+// // });
+
+// const api = axios.create({
+//   baseURL: "https://campus-maintainace-system-67db.vercel.app/api",
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://campus-maintainace-system-ly4tb7bsf-yraghuwanshi124-art.vercel.app/",
 });
 
 export default api;
